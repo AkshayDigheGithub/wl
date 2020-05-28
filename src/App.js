c4286a586ce82ef0a08faacbdb7805ed23e5884a
akshaydighe0710 <akshaydighe@rapidera.com>
2020-05-28 17:38:57 +0530

    basic login page added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
 import Skelton from "./components/skelton/index";
 import Home from "./components/home/index";
+import LoginComponent from "./components/login/login.component";
 import "./App.css";
+import { Container } from "react-bootstrap";
 
 function App() {
+  const isLogin = sessionStorage.getItem("login");
   return (
     <div className="App">
-      <Skelton>
-        <Home />
-      </Skelton>
+      <Container>
+        {/* this is single componet after login and sign up this skelton will call */}
+
+        {isLogin ? (
+          <Skelton>
+            <Home />
+          </Skelton>
+        ) : (
+          <LoginComponent />
+        )}
+      </Container>
     </div>
   );
 }
--- src/components/login/login.component.js
+++ src/components/login/login.component.js
@@ -1,13 +1,79 @@
 import React, { Component } from "react";
-import Skelton from "../skelton/index";
-
+import {
+  Form,
+  Container,
+  Row,
+  Col,
+  FormCheck,
+  Button,
+  ButtonToolbar,
+} from "react-bootstrap";
+import "./login.css";
 class LoginComponent extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.onSubmitHandle = this.onSubmitHandle.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.name === "email" ? target.value : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  onSubmitHandle(event) {
+    event.preventDefault();
+    console.log(this.state);
+    sessionStorage.setItem("login", true);
+  }
+
   render() {
     return (
-      <>
-        <h6>Login </h6>
-      </>
+      <Row>
+        <Col md={{ offset: 3, span: 6 }}>
+          <h2>Login Page</h2>
+          <Form onSubmit={this.onSubmitHandle}>
+            <Form.Group controlId="exampleForm.ControlInput1">
+              <Form.Control
+                type="email"
+                placeholder="name@example.com"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleInputChange}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Control
+                checked
+                type="password"
+                placeholder="Enter password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleInputChange}
+              />
+            </Form.Group>
+            <ButtonToolbar>
+              <Button
+                variant="primary"
+                block
+                className="login-button"
+                type="submit"
+              >
+                Login
+              </Button>
+            </ButtonToolbar>
+          </Form>
+        </Col>
+      </Row>
     );
   }
 }
